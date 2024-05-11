f51b00dfcc25cd28d5a86a2f2f948371016be429
pranavbansal2809 <pranavbansal2809@gmail.com>
2024-05-11 15:54:16 +0530

    Added new logo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import reportWebVitals from './reportWebVitals';
