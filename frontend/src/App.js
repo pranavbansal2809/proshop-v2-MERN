4ee5a4ddd477cdb28284340a83a4a60d94cd22ac
pranavbansal2809 <pranavbansal2809@gmail.com>
2024-05-12 11:21:10 +0530

    dding routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 //import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen"
+import { Outlet } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
 
 const App = () => {
   return (
     <>
       <Header/>
       <main className='py-3'>
-        <HomeScreen/>
+        <Container>
+          <Outlet/>
+        </Container>
+        
       </main>
       <Footer/>
     </>
