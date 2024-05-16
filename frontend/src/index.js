7d9443d1aa6e15ba82e0c0024371374ee345db49
pranavbansal2809 <pranavbansal2809@gmail.com>
2024-05-16 16:55:27 +0530

    Added product screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,11 +12,13 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true}  path="/"  element={<HomeScreen/>}/>
+      <Route  path="/product/:id"  element={<ProductScreen/>}/>
     </Route>
   )
 )
