af64454d738b1d75c18243d81dd0d53778f11c8c
pranavbansal2809 <pranavbansal2809@gmail.com>
2024-05-16 22:53:25 +0530

    Updated

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from "../components/Rating"
 import products from "../products"
 
