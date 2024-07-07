c58df6c7fac75d1ed940fd5bdf4a84650fe8ef44
pranavbansal2809 <pranavbansal2809@gmail.com>
2024-07-07 10:10:32 +0530

    Adding Custom Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,23 @@
 import express from 'express'
-import products from '../data/products.js'
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js'
 
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.get('/', asyncHandler(async(req,res)=>{
+    const products = await Product.find({});
     res.json(products);
-})
+}))
 
-router.get('/:id', (req,res)=>{
-    const product = products.find((p)=>p._id === req.params.id)
-    res.json(product)
-})
+router.get('/:id', asyncHandler(async(req,res)=>{
+    const product = await Product.findById(req.params.id);
+    if(product){
+        res.json(product);
+    }else{
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+    
+}))
 
 export default router;
