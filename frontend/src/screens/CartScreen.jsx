ab9cc12b0ca389a3114221ac4c2fe0f2fc6b605a
pranavbansal2809 <pranavbansal2809@gmail.com>
2024-07-11 14:34:15 +0530

    Check new changes

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,13 @@
+import {Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import {Message} from '../components/Message'
+
+const CartScreen = () => {
+  return (
+    <div>CartScreen</div>
+  )
+}
+
+export default CartScreen
